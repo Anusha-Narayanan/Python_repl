ff5e64c66f4b90a99cdcf79f1e0c20bcd2bb0e36
shirinarayanan <26742967-shirinarayanan@users.noreply.replit.com>
2023-09-22 14:50:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Write your code below this line 👇
+
+#print hello world
+print("Hello World!")
+
+#print 3 hello worlds in a single cmd instead of 3 different print statements
+print("Hello World!\nHello World!\nHello World!")
+
+#print statement with space between strings
+print("Hello" + " " + "Anusha") #string concatenation
+
+#print statement with a user input
+print("Hello " + input("what is your name?") + "!")
